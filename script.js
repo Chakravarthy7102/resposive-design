f5b7aa0e8e9fb5a019c27e633771c1ef74968c05
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2021-11-03 18:43:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const mobileBtn=document.getElementById('mobile-cta')
+nav=document.querySelector('nav')
+const mobileBtnExit=document.getElementById('mobile-exit');
+mobileBtn.addEventListener('click',()=>{
+    nav.classList.add('mobile-menu');
+});
+mobileBtnExit.addEventListener('click',()=>{
+    nav.classList.remove('mobile-menu');
+})
